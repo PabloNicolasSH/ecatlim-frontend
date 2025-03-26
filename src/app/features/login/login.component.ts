07198c3aae9503307a08420c985ec4b9efc46d51
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-03-26 15:19:40 +0000

    ECL-6 Completed login component

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Button} from 'primeng/button';
 import {FloatLabel} from 'primeng/floatlabel';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {InputText} from 'primeng/inputtext';
+import {Password} from 'primeng/password';
+import {Router, RouterLink} from '@angular/router';
+import {UserToLog} from '../../core/auth/user-to-log.model';
+import {AuthService} from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +14,43 @@ import {InputText} from 'primeng/inputtext';
     FloatLabel,
     FormsModule,
     InputText,
-    Button
+    Button,
+    ReactiveFormsModule,
+    Password,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-  email: string | undefined;
-  password: string | undefined;
+export class LoginComponent  implements OnInit{
+
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  protected loginForm!: FormGroup;
+  protected loading: boolean = false;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        username: ["", [Validators.required, Validators.email]],
+        password: ["", Validators.required]
+      }
+    )
+  }
+
+  login(){
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.valid && !this.loading){
+      this.loading = true;
+      const userToLog: UserToLog = {...this.loginForm.value};
+      this.authService.login(userToLog).subscribe({
+        next: () => this.router.navigate(['/app/home']),
+        error: (err) => {
+            this.loading = false;
+        }
+      });
+    }
+  }
 }
