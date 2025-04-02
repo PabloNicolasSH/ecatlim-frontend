02a6568bdbbf8c58b0404a629ef7d032614799cc
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-04-02 23:52:42 +0100

    ECL-6 Created ScoutGroup service and updated the user one

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {User} from '../models/user.model';
 import {environment} from '../../../environments/environment';
+import {UserForm} from '../models/user-form.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,26 @@ export class UserService {
   protected readonly http = inject(HttpClient);
 
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(`${environment.apiUrl}/user/admin/all`);
+    return this.http.get<User[]>(`${environment.apiUrl}/user/admin/allActives`);
+  }
+
+  getInactiveUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${environment.apiUrl}/user/admin/allInactives`);
+  }
+
+  addUser(user: UserForm): Observable<User>{
+    return this.http.post<User>(`${environment.apiUrl}/user/admin/add`, user);
+  }
+
+  updateUser(id: number, user: UserForm): Observable<User>{
+    return this.http.put<User>(`${environment.apiUrl}/user/admin/edit/${id}`, user);
+  }
+
+  deactivateUser(id: number): Observable<User> {
+    return this.http.delete<User>(`${environment.apiUrl}/user/admin/deactivate/${id}`);
+  }
+
+  activateUser(id: number): Observable<User> {
+    return this.http.put<User>(`${environment.apiUrl}/user/admin/activate/${id}`, {});
   }
 }
