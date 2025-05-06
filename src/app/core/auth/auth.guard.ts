7769badf0542eb37856ebedcd9a4127e302229be
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-05-06 10:14:50 +0100

    ECL-15 Updated token interceptor

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -1,4 +1,4 @@
-import {CanActivateFn, RedirectFunction, Router} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
 
--- src/app/core/auth/token.interceptor.ts
+++ src/app/core/auth/token.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
+import {catchError} from 'rxjs';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
@@ -12,8 +13,13 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(cloned)
+    return next(cloned).pipe(catchError(err=> {
+      if (err.status == 401){
+        authService.logout();
+      }
+      throw err;
+    }));
   }
-  
+
   return next(req);
 };
