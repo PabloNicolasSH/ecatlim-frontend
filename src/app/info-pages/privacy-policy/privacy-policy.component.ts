41469f5611a5b50ca2a7c487571221f8eeb510ff
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-05-06 20:02:45 +0100

    ECL-7 Created privacy policy page

---
+++ src/app/info-pages/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-privacy-policy',
+  imports: [
+    Button,
+    RouterLink
+  ],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+
+  protected readonly history = history;
+}
