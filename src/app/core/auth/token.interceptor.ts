74c5f738a4746e3ec10962ca3536b0b8648f6598
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2025-06-04 13:25:36 +0100

    Updated token interceptor to solve login problem

--- src/app/core/auth/token.interceptor.ts
+++ src/app/core/auth/token.interceptor.ts
@@ -2,24 +2,27 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
 import {catchError} from 'rxjs';
+import {Router} from '@angular/router';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
 
-  if (token){
-    const cloned = req.clone({
+  const authReq = token
+    ? req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
-    });
-    return next(cloned).pipe(catchError(err=> {
-      if (err.status == 401){
+    })
+    : req;
+
+  return next(authReq).pipe(
+    catchError(err => {
+      if (err.status === 401) {
         authService.logout();
       }
       throw err;
-    }));
-  }
-
-  return next(req);
+    })
+  );
 };
+
