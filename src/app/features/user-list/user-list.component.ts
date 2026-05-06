c2dbcdc9fb4e5884c516df52cb9254280142fd20
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2026-05-06 22:21:51 +0100

    ECL-19 Solved several issues and warnings

--- src/app/features/user-list/user-list.component.ts
+++ src/app/features/user-list/user-list.component.ts
@@ -26,8 +26,7 @@ import {ScoutGroupService} from '../../shared/services/scout-group.service';
     Button,
     UserModalAddEditComponent,
     ConfirmDialog,
-    FormsModule,
-    MultiSelect
+    FormsModule
   ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
