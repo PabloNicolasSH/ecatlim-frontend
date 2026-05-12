2313cafd7b604d352ae5dd487264dcf9b21b8ac9
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2026-05-13 00:10:05 +0100

    ECL-8 Updated event calendar and added enrollment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,6 +9,10 @@ import {MyPreset} from './mypreset'
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {tokenInterceptor} from './core/auth/token.interceptor';
 import {ConfirmationService, MessageService} from 'primeng/api';
+import {registerLocaleData} from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs)
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -41,6 +45,10 @@ export const appConfig: ApplicationConfig = {
         dateFormat: 'dd/mm/yy',
         firstDayOfWeek: 1
       }
-    })
+    }),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-ES'
+    }
   ]
 };
--- src/app/features/event-calendar/event-calendar.component.ts
+++ src/app/features/event-calendar/event-calendar.component.ts
@@ -1,25 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import {CalendarOptions} from '@fullcalendar/core';
 import {FullCalendarModule} from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import {EventService} from '../../shared/services/event.service';
+import {EventCalendar} from '../../shared/models/event.model';
+import {DatePipe} from '@angular/common';
+import {Button} from 'primeng/button';
+import {Tag} from 'primeng/tag';
+import {Dialog} from 'primeng/dialog';
+import {MessageService, PrimeTemplate} from 'primeng/api';
+import {Chip} from 'primeng/chip';
 
 @Component({
   selector: 'app-event-calendar',
   imports: [
-    FullCalendarModule
+    FullCalendarModule,
+    DatePipe,
+    Button,
+    Tag,
+    Dialog,
+    PrimeTemplate,
+    Chip
   ],
   templateUrl: './event-calendar.component.html',
   styleUrl: './event-calendar.component.scss'
 })
-export class EventCalendarComponent {
+export class EventCalendarComponent implements OnInit{
+
+  protected readonly eventService = inject(EventService);
+  protected readonly messageService = inject(MessageService);
+
+  userEvents = signal<EventCalendar[]>([]);
+  allEvents = signal<EventCalendar[]>([]);
+  showingAllEvents = signal(false);
+  showModal = signal(false);
+  selectedEvent = signal<any>(null);
+
+  isMultiDay = computed(() => {
+    const event = this.selectedEvent();
+    if (!event) return false;
+
+    const start = new Date(event.start);
+    const end = new Date(event.end);
+
+    return start.toDateString() !== end.toDateString();
+  });
 
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
+    firstDay: 1,
     customButtons:{
       seeAllEventsButton: {
         text: 'Ver todos los eventos',
-        click: () => this.seeAllEvents()
+        click: () => this.toggleView()
       }
     },
     headerToolbar: {
@@ -31,6 +65,7 @@ export class EventCalendarComponent {
       seeAllEventsButton: 'Ver'
     },
     events: [],
+    eventClick: (info) => this.handleEventClick(info),
     eventTimeFormat: {
       hour: '2-digit',
       minute: '2-digit',
@@ -41,7 +76,90 @@ export class EventCalendarComponent {
     height: 'auto',
   };
 
-  private seeAllEvents() {
-    console.log("Ver todos los eventos");
+  ngOnInit(): void {
+    this.loadEvents();
+  }
+
+  loadEvents() {
+    this.eventService.getEventsForCalendar()
+    .subscribe({
+      next: events => {
+        const mappedEvents = events.map(event => ({
+          ...event,
+          id: event.id?.toString(),
+          start: event.startDate,
+          end: event.endDate,
+          classNames: event.isCurrentUserAttending ? ['event-enrolled'] : [],
+          title: event.isCurrentUserAttending ? `✓ ${event.title}` : event.title,
+          backgroundColor: this.getColor(event.educationStageCode),
+          borderColor: this.getColor(event.educationStageCode)
+        }));
+        this.allEvents.set(mappedEvents);
+        this.userEvents.set(mappedEvents.filter(e => e.isCurrentUserAttending || e.canParticipate));
+        this.calendarOptions = {
+          ...this.calendarOptions,
+          events: this.userEvents()
+        };
+      }
+    });
+  }
+
+  toggleView() {
+    this.showingAllEvents.update(prev => !prev);
+    this.updateCalendarEvents();
+  }
+
+  private updateCalendarEvents() {
+    this.calendarOptions = {
+      ...this.calendarOptions,
+      events: this.showingAllEvents() ? this.allEvents() : this.userEvents(),
+      customButtons: {
+        seeAllEventsButton: {
+          text: this.showingAllEvents() ? 'Ver mis eventos' : 'Ver todos los eventos',
+          click: () => this.toggleView()
+        }
+      }
+    };
+  }
+
+  handleEventClick(info: any) {
+    this.selectedEvent.set({
+      id: Number(info.event.id),
+      title: info.event.title,
+      start: info.event.start,
+      end: info.event.end,
+      ...info.event.extendedProps
+    });
+    this.showModal.set(true);
+  }
+
+  registerToEvent(eventId: number) {
+    this.eventService.enroll(eventId)
+      .subscribe({
+        next: () => {
+          this.showModal.set(false);
+          this.loadEvents();
+          this.messageService.add({
+            summary: "¡Te has inscrito correctamente al evento!",
+            detail: "Acuérdate, la formación empieza el " + this.selectedEvent().start.toLocaleDateString(),
+            severity: 'success',
+          })
+        },
+        error: () => {
+          this.messageService.add({
+            text: "",
+            severity: 'error',
+          })
+        }
+      });
+  }
+
+  private getColor(educationStageCode: string | undefined) {
+    switch (educationStageCode) {
+      case 'AS': return 'var(--color-blue-500)';
+      case 'ES': return 'var(--color-purple-600)';
+      case 'CS': return 'var(--color-ecatlim)';
+      default: return 'var(--color-green-600)';
+    }
   }
 }
