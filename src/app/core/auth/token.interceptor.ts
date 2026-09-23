f5f5bb5ab1645b1a7c330f3f85662f5088356cd3
Pablo Nicolás <pablo.santana116@alu.ulpgc.es>
2026-09-23 17:53:59 +0100

    ECL-24 Added general errors interceptor

--- src/app/core/auth/token.interceptor.ts
+++ src/app/core/auth/token.interceptor.ts
@@ -2,11 +2,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
 import {catchError} from 'rxjs';
-import {Router} from '@angular/router';
+import {MessageService} from 'primeng/api';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
+
+  const messageService = inject(MessageService);
 
   const authReq = token
     ? req.clone({
@@ -20,6 +22,17 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
     catchError(err => {
       if (err.status === 401) {
         authService.logout();
+        messageService.add({
+          severity: 'warn',
+          summary: 'Aviso',
+          detail: "Vuelva a iniciar sesión"
+        })
+      } else {
+        messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: err.error.ecatlimMessage?? "Ha ocurrido un error inesperado"
+        })
       }
       throw err;
     })
